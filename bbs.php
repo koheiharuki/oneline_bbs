9bce747d6b082604dfadaefc01547d6179eea56c
NOTE-HARUKI-PRI\kohei <koheiaharuki@gmail.com>
2017-04-25 09:54:11 +0900

    ウイニングイレブン仕様にした

--- bbs.php
+++ bbs.php
@@ -1,17 +1,17 @@
 <?php
 
 	// データベースに接続local
-	// $dns = 'mysql:dbname=oneline_bbs;host=localhost';
-	// $user = 'root';
-	// $password = '';
+	$dns = 'mysql:dbname=oneline_bbs;host=localhost';
+	$user = 'root';
+	$password = '';
 
 	// データベースに接続remote
 	// dbnameをロリポップのデータベース名に、hostをロリポップのサーバーに変更
-	$dsn = 'mysql:dbname=LAA0854002-onelinebbs;host=mysql108.phy.lolipop.lan';
+	// $dsn = 'mysql:dbname=LAA0854002-onelinebbs;host=mysql108.phy.lolipop.lan';
 // userをロリポップのユーザー名に変更
-	$user = 'LAA0854002';
+	// $user = 'LAA0854002';
 // 	passwordをロリポップのパスワードに変更
-	$password = 'khkh3374';
+	// $password = 'khkh3374';
 
 	$dbh = new PDO($dns,$user,$password);
 	$dbh->query('SET NAMES utf8');
@@ -61,7 +61,7 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>セブ掲示版</title>
+  <title>ウイニングイレブン掲示板</title>
 
   <!-- CSS -->
   <link rel="stylesheet" href="assets/css/bootstrap.css">
@@ -82,9 +82,10 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#page-top"><span class="strong-title"><i class="fa fa-linux"></i> Oneline bbs</span></a>
+              <a class="navbar-brand" href="#page-top"><span class="strong-title"><i class="fa fa-futbol-o" aria-hidden="true"></i> Winning Eleven BBS</span></a>
           </div>
           <!-- Collect the nav links, forms, and other content for toggling -->
+
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav navbar-right">
               </ul>
@@ -101,26 +102,47 @@
 
       <!-- 画面左側 -->
       <div class="col-md-4 content-margin-top">
+				<div style="margin-bottom: 20px; margin-top:0; ">みんなで情報共有しましょう！！！</div>
         <!-- form部分 -->
         <form action="bbs.php" method="post">
           <!-- nickname -->
           <div class="form-group">
             <div class="input-group">
-              <input type="text" name="nickname" class="form-control" id="validate-text" placeholder="nickname" required>
+              <input type="text" name="nickname" class="form-control" id="validate-text" placeholder="ニックネーム" required>
               <span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
             </div>
           </div>
           <!-- comment -->
           <div class="form-group">
             <div class="input-group" data-validate="length" data-length="4">
-              <textarea type="text" class="form-control" name="comment" id="validate-length" placeholder="comment" required></textarea>
+              <textarea type="text" class="form-control" name="comment" id="validate-length" placeholder="レビューを書く" required></textarea>
               <span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
             </div>
           </div>
           <!-- つぶやくボタン -->
-          <button type="submit" class="btn btn-primary col-xs-12" disabled>つぶやく</button>
+          <button type="submit" class="btn btn-primary col-xs-12" disabled　>投稿する</button>
         </form>
+				<br><br><br><br>
+				<div class="glyphicon glyphicon-circle-arrow-down">公式サイト集
+
+				</div>
+				<a href="https://www.konami.com/wepes/2017/jp/ja/"><img src="" alt="">
+	<img src="assets/img/win2017.png" alt="win2017.png" width="360" height"60"></a>
+
+	<br><br>
+
+	<a href="http://www.konami.jp/we/2016/index.php5"><img src="" alt="">
+<img src="assets/img/win2016.png" alt="win2016.png" width="360" height"60"></a>
+
+<br><br>
+
+<a href="http://www.konami.jp/we/2015/"><img src="" alt="">
+<img src="assets/img/win2015.jpg" alt="win2015.png" width="360" height"60"></a>
+
+
+
       </div>
+
 
       <!-- 画面右側 -->
       <div class="col-md-8 content-margin-top">
@@ -129,11 +151,11 @@
 										foreach ($post_datas as $post_each) {?>
           <article class="timeline-entry">
               <div class="timeline-entry-inner">
-                  <div class="timeline-icon bg-success">
+                  <div class="timeline-icon bg-success" style="background-color: black">
                       <i class="entypo-feather"></i>
-                      <i class="fa fa-cogs"></i>
+                      <i class="fa fa-futbol-o" aria-hidden="true" style="font-size:40px"></i>
                   </div>
-                  <div class="timeline-label">
+                  <div class="timeline-label" style="background-color: #e3ffe0">
                       <h2><a href="#"><?php echo $post_each['nickname'] . '<br>'; ?></a> <span><?php echo $post_each['comment'] . '<br>'; ?></span></h2>
 											<?php
 										   //一旦日時型に変換（string型からdatetime型へ変換）
